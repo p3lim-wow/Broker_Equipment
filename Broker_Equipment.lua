b70fc1ac13121cdd3126bb1a7b523c992de2dba2
Adrian L Lange <adrianlund@gmail.com>
2009-09-13 00:32:27 +0200

    Clean up localization system too

--- Broker_Equipment.lua
+++ Broker_Equipment.lua
@@ -13,27 +13,27 @@ local L = {}
 if(GetLocale() == 'deDE') then -- Katharsis / copystring
 	L.NOSET = 'Kein set'
 	L.TOOLTIP = 'Klicke links um dein set zu ändern\nKlicke rechts um den GearManager zu öffnen'
-	L.HINTS = {' ', '|cff00ff00Shift-klicke um den set zu aktualisieren\nStrg-klicke um den set zu löschen|r'}
+	L.HINTS =  '|cff00ff00Shift-klicke um den set zu aktualisieren\nStrg-klicke um den set zu löschen|r'
 elseif(GetLocale() == 'frFR') then -- Soeters / Gnaf
 	L.NOSET = 'Pas de set'
 	L.TOOLTIP = 'Clic gauche pour changer d\'équipement\nClic droit pour ouvrir le gestionnaire d\'équipement'
-	L.HINTS = {' ', '|cff00ff00Maj-clic pour mettre à jour le set\nCtrl-clic pour supprimer le set|r'}
+	L.HINTS = '|cff00ff00Maj-clic pour mettre à jour le set\nCtrl-clic pour supprimer le set|r'
 elseif(GetLocale() == 'zhCN') then -- yleaf
 	L.NOSET = '无套装'
 	L.TOOLTIP = '左键点击切换套装\n右键打开套装管理器'
-	L.HINTS = {' ', '|cff00ff00Shift点击覆盖套装\nCtrl点击删除套装|r'}
+	L.HINTS = '|cff00ff00Shift点击覆盖套装\nCtrl点击删除套装|r'
 elseif(GetLocale() == 'zhTW') then -- yleaf
 	L.NOSET = '無套裝'
 	L.TOOLTIP = '左鍵點擊切換套裝\n右鍵點擊打開套裝管理器'
-	L.HINTS = {' ', '|cff00ff00Shift點擊覆蓋套裝\nCtrl點擊刪除套裝|r'}
+	L.HINTS = '|cff00ff00Shift點擊覆蓋套裝\nCtrl點擊刪除套裝|r'
 elseif(GetLocale() == 'koKR') then -- mrgyver
 	L.NOSET = '세트 없음'
 	L.TOOLTIP = '좌-클릭 세트 변경\n우-클릭 장비 관리창 열기'
-	L.HINTS = {' ', '|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r'}
+	L.HINTS = '|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r'
 else
 	L.NOSET = 'No set'
 	L.TOOLTIP = 'Left-click to change your set\nRight-click to open GearManager'
-	L.HINTS = {' ', '|cff00ff00Shift-click to update set\nCtrl-click to delete set|r'}
+	L.HINTS = '|cff00ff00Shift-click to update set\nCtrl-click to delete set|r'
 end
 
 
@@ -118,7 +118,7 @@ local function updateMenu()
 	for index = 1, GetNumEquipmentSets() do
 		local name, icon = GetEquipmentSetInfo(index)
 		temp = {
-			notCheckable = true,
+			notCheckable = 1,
 			text = name,
 			icon = icon,
 			func = function() menuClick(name, icon) end
@@ -126,14 +126,19 @@ local function updateMenu()
 		table.insert(menu, temp)
 	end
 
-	for index = 1, 2 do
-		temp = {
-			text = L.HINTS[index],
-			notCheckable = true,
-			disabled = true
-		}
-		table.insert(menu, temp)
-	end
+	temp = {
+		text = ' ',
+		notCheckable = 1,
+		disabled = 1
+	}
+	table.insert(menu, temp)
+
+	temp = {
+		text = L.HINTS,
+		notCheckable = 1,
+		disabled = 1
+	}
+	table.insert(menu, temp)
 end
 
 -- Fuck blizzard!
