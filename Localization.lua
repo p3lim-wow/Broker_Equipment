a8a69da195ae99ccbf096664d81e52763ee3216c
Adrian L Lange <adrianlund@gmail.com>
2009-12-15 23:25:39 +0100

    Change the table layout for all localizations

--- Localization.lua
+++ Localization.lua
@@ -5,21 +5,21 @@ addonTable.L = GetLocale() == 'deDE' and {-- Katharsis / copystring
 	'Klicke links um dein set zu ändern\nKlicke rechts um den GearManager zu öffnen',
 	'|cff00ff00Shift-klicke um den set zu aktualisieren\nStrg-klicke um den set zu löschen|r',
 } or GetLocale() == 'frFR' and { -- Soeters / Gnaf
-	NOSET = 'Pas de set',
-	TOOLTIP = 'Clic gauche pour changer d\'équipement\nClic droit pour ouvrir le gestionnaire d\'équipement',
-	HINTS = '|cff00ff00Maj-clic pour mettre à jour le set\nCtrl-clic pour supprimer le set|r',
+	'Pas de set',
+	'Clic gauche pour changer d\'équipement\nClic droit pour ouvrir le gestionnaire d\'équipement',
+	'|cff00ff00Maj-clic pour mettre à jour le set\nCtrl-clic pour supprimer le set|r',
 } or GetLocale() == 'zhCN' and { -- yleaf
-	NOSET = '无套装',
-	TOOLTIP = '左键点击切换套装\n右键打开套装管理器',
-	HINTS = '|cff00ff00Shift点击覆盖套装\nCtrl点击删除套装|r',
+	'无套装',
+	'左键点击切换套装\n右键打开套装管理器',
+	'|cff00ff00Shift点击覆盖套装\nCtrl点击删除套装|r',
 } or GetLocale() == 'zhTW' and { -- yleaf
-	NOSET = '無套裝',
-	TOOLTIP = '左鍵點擊切換套裝\n右鍵點擊打開套裝管理器',
-	HINTS = '|cff00ff00Shift點擊覆蓋套裝\nCtrl點擊刪除套裝|r',
+	'無套裝',
+	'左鍵點擊切換套裝\n右鍵點擊打開套裝管理器',
+	'|cff00ff00Shift點擊覆蓋套裝\nCtrl點擊刪除套裝|r',
 } or GetLocale() == 'koKR' and { -- mrgyver
-	NOSET = '세트 없음',
-	TOOLTIP = '좌-클릭 세트 변경\n우-클릭 장비 관리창 열기',
-	HINTS = '|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r',
+	'세트 없음',
+	'좌-클릭 세트 변경\n우-클릭 장비 관리창 열기',
+	'|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r',
 } or {
 	'No set',
 	'Left-click to change your set\nRight-click to open GearManager',
