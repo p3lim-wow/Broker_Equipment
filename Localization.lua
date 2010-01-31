307c0b9ecbfc64d6d1caa0312f984c01d30482d6
Adrian L Lange <adrianlund@gmail.com>
2010-01-31 20:20:02 +0100

    Re-do localization credits

--- Localization.lua
+++ Localization.lua
@@ -1,22 +1,26 @@
-﻿
+﻿--[[
+	Localization submitted through CurseForge
+	http://wow.curseforge.com/addons/broker_equipment/localization/
+--]]
+
 local _, ns = ...
-ns.L = GetLocale() == 'deDE' and {-- Katharsis / copystring
+ns.L = GetLocale() == 'deDE' and {
 	'Kein set',
 	'Klicke links um dein set zu ändern\nKlicke rechts um den GearManager zu öffnen',
 	'|cff00ff00Shift-klicke um den set zu aktualisieren\nStrg-klicke um den set zu löschen|r',
-} or GetLocale() == 'frFR' and { -- Soeters / Gnaf
+} or GetLocale() == 'frFR' and {
 	'Pas de set',
 	'Clic gauche pour changer d\'équipement\nClic droit pour ouvrir le gestionnaire d\'équipement',
 	'|cff00ff00Maj-clic pour mettre à jour le set\nCtrl-clic pour supprimer le set|r',
-} or GetLocale() == 'zhCN' and { -- yleaf
+} or GetLocale() == 'zhCN' and {
 	'无套装',
 	'左键点击切换套装\n右键打开套装管理器',
 	'|cff00ff00Shift点击覆盖套装\nCtrl点击删除套装|r',
-} or GetLocale() == 'zhTW' and { -- yleaf
+} or GetLocale() == 'zhTW' and {
 	'無套裝',
 	'左鍵點擊切換套裝\n右鍵點擊打開套裝管理器',
 	'|cff00ff00Shift點擊覆蓋套裝\nCtrl點擊刪除套裝|r',
-} or GetLocale() == 'koKR' and { -- mrgyver
+} or GetLocale() == 'koKR' and {
 	'세트 없음',
 	'좌-클릭 세트 변경\n우-클릭 장비 관리창 열기',
 	'|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r',
