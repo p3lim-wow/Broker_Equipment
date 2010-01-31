875d4b9dda40dd1b01f2b7cdf96ee2659a6ab832
Adrian L Lange <adrianlund@gmail.com>
2010-01-31 20:26:14 +0100

    Typo

--- Localization.lua
+++ Localization.lua
@@ -26,7 +26,7 @@ ns.L = GetLocale() == 'deDE' and {
 	'|cff00ff00Shift-클릭 하면 세트 업데이트\nCtrl-클릭 하면 세트 삭제|r',
 } or GetLocale() == 'esES' and {
 	'Sin set',
-	'Click izquierdo para combiar tu set\nClick derecho para abrir el Administrador de Equipo',
+	'Click izquierdo para cambiar tu set\nClick derecho para abrir el Administrador de Equipo',
 	'|cff00ff00Shift-click para modificar set\nCtrl-click para eliminar set',
 } or GetLocale() == 'esMX' and {
 	'Sin set',
