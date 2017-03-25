9b3e62bde7944c89f69438af60307f3486c87908
Adrian L Lange <mail@p3lim.net>
2017-03-26 00:51:39 +0100

    Repurpose the event frame as the menu

--- Broker_Equipment.lua
+++ Broker_Equipment.lua
@@ -1,153 +1,34 @@
-﻿local _, L = ...
+﻿local addonName, L = ...
 
-local Broker_Equipment = CreateFrame('Frame')
-Broker_Equipment:RegisterEvent('PLAYER_LOGIN')
-Broker_Equipment:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
-
+local QUESTION_MARK_ICON = [[Interface\Icons\INV_MISC_QUESTIONMARK]]
 local BACKDROP = {
 	bgFile = [[Interface\Tooltips\UI-Tooltip-Background]],
 	edgeFile = [[Interface\Tooltips\UI-Tooltip-Border]], edgeSize = 16,
 	insets = {top = 3, bottom = 3, left = 3, right = 3}
 }
 
+local items, pending = {}
 local LDB = LibStub('LibDataBroker-1.1'):NewDataObject('Broker_Equipment', {
 	type = 'data source',
 })
 
-local Menu, pending
+local Broker_Equipment = CreateFrame('Frame', addonName, UIParent)
+Broker_Equipment:RegisterEvent('PLAYER_LOGIN')
+Broker_Equipment:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
+Broker_Equipment:Hide()
 
-local function UpdateDisplay()
-	if(InCombatLockdown() and pending) then
-		LDB.text = '|cffffff00' .. pending
-		LDB.icon = [[Interface\Icons\]] .. GetEquipmentSetInfoByName(pending)
-	else
-		for index = 1, GetNumEquipmentSets() do
-			local name, icon, _, equipped = GetEquipmentSetInfo(index)
-			if(equipped) then
-				LDB.text = name
-				LDB.icon = icon or [[Interface\Icons\INV_MISC_QUESTIONMARK]]
-				return
-			end
-		end
+local Fader = Broker_Equipment:CreateAnimationGroup()
+Fader:CreateAnimation():SetDuration(UIDROPDOWNMENU_SHOW_TIME)
+Fader:SetScript('OnFinished', function()
+	Broker_Equipment:Hide()
+end)
 
-		LDB.text = UNKNOWN
-		LDB.icon = [[Interface\Icons\INV_MISC_QUESTIONMARK]]
-	end
+local function FadePause()
+	Fader:Stop()
 end
 
-local function OnItemClick(self)
-	if(IsShiftKeyDown() and not pending) then
-		local dialog = StaticPopup_Show('CONFIRM_SAVE_EQUIPMENT_SET', self.name)
-		dialog.data = self.name
-	elseif(IsControlKeyDown() and not pending) then
-		local dialog = StaticPopup_Show('CONFIRM_DELETE_EQUIPMENT_SET', self.name)
-		dialog.data = self.name
-	else
-		if(InCombatLockdown()) then
-			Broker_Equipment:RegisterEvent('PLAYER_REGEN_ENABLED')
-			pending = self.name
-
-			UpdateDisplay()
-		else
-			EquipmentManager_EquipSet(pending or self.name)
-		end
-	end
-
-	Menu:Hide()
-end
-
-local function OnEnter()
-	Menu.Fader:Stop()
-end
-
-local function OnLeave()
-	Menu.Fader:Play()
-end
-
-local function OnItemEnter(self)
-	if(Broker_EquipmentDB.showTooltipMenu) then
-		GameTooltip:SetOwner(self, 'ANCHOR_RIGHT')
-		GameTooltip:SetEquipmentSet(self.name)
-
-		GameTooltip:AddLine(' ')
-		GameTooltip:AddLine(L['|cff33ff33Click|r to equip set.'])
-		GameTooltip:AddLine(L['|cff33ff33Shift-Click|r to update set with current equipment.'])
-		GameTooltip:AddLine(L['|cff33ff33Ctrl-Click|r to |cffff3333delete|r set.'])
-		GameTooltip:Show()
-	end
-
-	OnEnter()
-end
-
-local function UpdateMenu(parent)
-	local maxWidth = 0
-
-	local numEquipmentSets = GetNumEquipmentSets()
-	for index = 1, numEquipmentSets do
-		local Item = Menu.items[index]
-		if(not Item) then
-			Item = CreateFrame('Button', nil, Menu)
-			Item:SetPoint('TOPLEFT', 11, -((index - 1) * 18) - UIDROPDOWNMENU_BORDER_HEIGHT)
-			Item:SetHighlightTexture([[Interface\QuestFrame\UI-QuestTitleHighlight]])
-			Item:GetHighlightTexture():SetBlendMode('ADD')
-			Item:SetScript('OnClick', OnItemClick)
-			Item:SetScript('OnEnter', OnItemEnter)
-			Item:SetScript('OnLeave', GameTooltip_Hide)
-			Item:HookScript('OnLeave', OnLeave)
-
-			local Button = CreateFrame('CheckButton', nil, Item)
-			Button:SetPoint('LEFT')
-			Button:SetSize(16, 16)
-			Button:SetNormalTexture([[Interface\Common\UI-DropDownRadioChecks]])
-			Button:GetNormalTexture():SetTexCoord(0.5, 1, 0.5, 1)
-			Button:SetCheckedTexture([[Interface\Common\UI-DropDownRadioChecks]])
-			Button:GetCheckedTexture():SetTexCoord(0, 0.5, 0.5, 1)
-			Button:EnableMouse(false)
-			Item.Button = Button
-
-			local Label = Item:CreateFontString(nil, nil, 'GameFontHighlightSmall')
-			Label:SetPoint('LEFT', 20, 0)
-			Item:SetFontString(Label)
-			Item.Label = Label
-
-			local Icon = Item:CreateTexture(nil, 'ARTWORK')
-			Icon:SetPoint('RIGHT')
-			Icon:SetSize(16, 16)
-			Item.Icon = Icon
-
-			Menu.items[index] = Item
-		else
-			Item:Show()
-		end
-
-		local name, icon, _, equipped, _, _, _, missing = GetEquipmentSetInfo(index)
-		Item.Button:SetChecked(equipped)
-		Item.Icon:SetTexture(icon or [[Interface\Icons\INV_MISC_QUESTIONMARK]])
-		Item.name = name
-
-		if(pending == name) then
-			Item:SetFormattedText('|cffffff00%s|r', name)
-		elseif(missing > 0) then
-			Item:SetFormattedText('|cffff0000%s|r', name)
-		else
-			Item:SetText(name)
-		end
-
-		local width = Item.Label:GetWidth() + 50
-		if(width > maxWidth) then
-			maxWidth = width
-		end
-	end
-
-	for index = numEquipmentSets + 1, #Menu.items do
-		Menu.items[index]:Hide()
-	end
-
-	for _, Item in next, Menu.items do
-		Item:SetSize(maxWidth, 18)
-	end
-
-	Menu:SetSize(maxWidth + 25, (numEquipmentSets * 18) + (UIDROPDOWNMENU_BORDER_HEIGHT * 2))
+local function FadeOut()
+	Fader:Play()
 end
 
 function LDB:OnTooltipShow()
@@ -166,53 +47,35 @@ function LDB:OnClick(button)
 	end
 
 	if(button ~= 'RightButton' and GetNumEquipmentSets() > 0) then
-		if(not Menu) then
-			Menu = CreateFrame('Frame', nil, UIParent)
-			Menu:SetBackdrop(BACKDROP)
-			Menu:SetBackdropColor(0, 0, 0)
-			Menu:SetFrameStrata('DIALOG')
-			Menu:SetScript('OnEnter', OnEnter)
-			Menu:SetScript('OnLeave', OnLeave)
-			Menu:Hide()
-			Menu.items = {}
-
-			local Fader = Menu:CreateAnimationGroup()
-			Fader:CreateAnimation():SetDuration(UIDROPDOWNMENU_SHOW_TIME)
-			Fader:SetScript('OnFinished', function()
-				Menu:Hide()
-			end)
-			Menu.Fader = Fader
-		end
-
-		if(Menu:IsShown()) then
-			Menu:Hide()
+		if(Broker_Equipment:IsShown()) then
+			Broker_Equipment:Hide()
 		else
-			UpdateMenu(self)
-			Menu:ClearAllPoints()
-			Menu:SetPoint('TOP', self, 'BOTTOM') -- temporary anchor
+			Broker_Equipment:Update()
+			Broker_Equipment:ClearAllPoints()
+			Broker_Equipment:SetPoint('TOP', self, 'BOTTOM') -- temporary anchor
 
 			local sideAnchor = ''
-			if(Menu:GetRight() > GetScreenWidth()) then
+			if(Broker_Equipment:GetRight() > GetScreenWidth()) then
 				sideAnchor = 'RIGHT'
-			elseif(Menu:GetLeft() <= 0) then
+			elseif(Broker_Equipment:GetLeft() <= 0) then
 				sideAnchor = 'LEFT'
 			end
 
-			Menu:ClearAllPoints()
-			if(Menu:GetBottom() <= 0) then
-				Menu:SetPoint('BOTTOM' .. sideAnchor, self, 'TOP' .. sideAnchor)
+			Broker_Equipment:ClearAllPoints()
+			if(Broker_Equipment:GetBottom() <= 0) then
+				Broker_Equipment:SetPoint('BOTTOM' .. sideAnchor, self, 'TOP' .. sideAnchor)
 			else
-				Menu:SetPoint('TOP' .. sideAnchor, self, 'BOTTOM' .. sideAnchor)
+				Broker_Equipment:SetPoint('TOP' .. sideAnchor, self, 'BOTTOM' .. sideAnchor)
 			end
 
-			Menu:Show()
+			Broker_Equipment:Show()
 		end
 
 		PlaySound('igMainMenuOptionCheckBoxOn')
 
 		if(not hooked[self]) then
-			self:HookScript('OnEnter', OnEnter)
-			self:HookScript('OnLeave', OnLeave)
+			self:HookScript('OnEnter', FadePause)
+			self:HookScript('OnLeave', FadeOut)
 
 			hooked[self] = true
 		end
@@ -232,17 +95,155 @@ function LDB:OnClick(button)
 	end
 end
 
+local function OnItemClick(self)
+	if(IsShiftKeyDown() and not pending) then
+		local dialog = StaticPopup_Show('CONFIRM_SAVE_EQUIPMENT_SET', self.name)
+		dialog.data = self.name
+	elseif(IsControlKeyDown() and not pending) then
+		local dialog = StaticPopup_Show('CONFIRM_DELETE_EQUIPMENT_SET', self.name)
+		dialog.data = self.name
+	else
+		if(InCombatLockdown()) then
+			Broker_Equipment:RegisterEvent('PLAYER_REGEN_ENABLED')
+			pending = self.name
+
+			Broker_Equipment:UpdateDisplay()
+		else
+			EquipmentManager_EquipSet(pending or self.name)
+		end
+	end
+
+	Broker_Equipment:Hide()
+end
+
+local function OnItemEnter(self)
+	if(Broker_EquipmentDB.showTooltipMenu) then
+		GameTooltip:SetOwner(self, 'ANCHOR_RIGHT')
+		GameTooltip:SetEquipmentSet(self.name)
+
+		GameTooltip:AddLine(' ')
+		GameTooltip:AddLine(L['|cff33ff33Click|r to equip set.'])
+		GameTooltip:AddLine(L['|cff33ff33Shift-Click|r to update set with current equipment.'])
+		GameTooltip:AddLine(L['|cff33ff33Ctrl-Click|r to |cffff3333delete|r set.'])
+		GameTooltip:Show()
+	end
+
+	Fader:Stop()
+end
+
+local function OnItemLeave(self)
+	GameTooltip_Hide(self)
+	Fader:Play()
+end
+
+function Broker_Equipment:CreateItem(index)
+	local Item = CreateFrame('Button', nil, self)
+	Item:SetPoint('TOPLEFT', 11, -((index - 1) * 18) - UIDROPDOWNMENU_BORDER_HEIGHT)
+	Item:SetHighlightTexture([[Interface\QuestFrame\UI-QuestTitleHighlight]])
+	Item:GetHighlightTexture():SetBlendMode('ADD')
+	Item:SetScript('OnClick', OnItemClick)
+	Item:SetScript('OnEnter', OnItemEnter)
+	Item:SetScript('OnLeave', OnItemLeave)
+
+	local Button = CreateFrame('CheckButton', nil, Item)
+	Button:SetPoint('LEFT')
+	Button:SetSize(16, 16)
+	Button:SetNormalTexture([[Interface\Common\UI-DropDownRadioChecks]])
+	Button:GetNormalTexture():SetTexCoord(0.5, 1, 0.5, 1)
+	Button:SetCheckedTexture([[Interface\Common\UI-DropDownRadioChecks]])
+	Button:GetCheckedTexture():SetTexCoord(0, 0.5, 0.5, 1)
+	Button:EnableMouse(false)
+	Item.Button = Button
+
+	local Label = Item:CreateFontString(nil, nil, 'GameFontHighlightSmall')
+	Label:SetPoint('LEFT', 20, 0)
+	Item:SetFontString(Label)
+	Item.Label = Label
+
+	local Icon = Item:CreateTexture(nil, 'ARTWORK')
+	Icon:SetPoint('RIGHT')
+	Icon:SetSize(16, 16)
+	Item.Icon = Icon
+
+	items[index] = Item
+
+	return Item
+end
+
+function Broker_Equipment:Update()
+	local maxWidth = 0
+
+	local numEquipmentSets = GetNumEquipmentSets()
+	for index = 1, numEquipmentSets do
+		local Item = items[index] or self:CreateItem(index)
+
+		local name, icon, _, equipped, _, _, _, missing = GetEquipmentSetInfo(index)
+		Item.Button:SetChecked(equipped)
+		Item.Icon:SetTexture(icon or [[Interface\Icons\INV_MISC_QUESTIONMARK]])
+		Item.name = name
+		Item:Show()
+
+		if(pending == name) then
+			Item:SetFormattedText('|cffffff00%s|r', name)
+		elseif(missing > 0) then
+			Item:SetFormattedText('|cffff0000%s|r', name)
+		else
+			Item:SetText(name)
+		end
+
+		local width = Item.Label:GetWidth() + 50
+		if(width > maxWidth) then
+			maxWidth = width
+		end
+	end
+
+	for index = numEquipmentSets + 1, #items do
+		items[index]:Hide()
+	end
+
+	for _, Item in next, items do
+		Item:SetSize(maxWidth, 18)
+	end
+
+	self:SetSize(maxWidth + 25, (numEquipmentSets * 18) + (UIDROPDOWNMENU_BORDER_HEIGHT * 2))
+end
+
+function Broker_Equipment:UpdateDisplay()
+	if(InCombatLockdown() and pending) then
+		LDB.text = '|cffffff00' .. pending
+		LDB.icon = [[Interface\Icons\]] .. GetEquipmentSetInfoByName(pending)
+	else
+		for index = 1, GetNumEquipmentSets() do
+			local name, icon, _, equipped = GetEquipmentSetInfo(index)
+			if(equipped) then
+				LDB.text = name
+				LDB.icon = icon or QUESTION_MARK_ICON
+				return
+			end
+		end
+
+		LDB.text = UNKNOWN
+		LDB.icon = QUESTION_MARK_ICON
+	end
+end
+
 function Broker_Equipment:PLAYER_LOGIN()
 	self:RegisterEvent('UNIT_INVENTORY_CHANGED')
 	self:RegisterEvent('EQUIPMENT_SETS_CHANGED')
-	self.EQUIPMENT_SETS_CHANGED = UpdateDisplay
+	self.EQUIPMENT_SETS_CHANGED = self.UpdateDisplay
 
-	UpdateDisplay()
+
+	self:SetBackdrop(BACKDROP)
+	self:SetBackdropColor(0, 0, 0)
+	self:SetFrameStrata('DIALOG')
+	self:SetScript('OnEnter', FadePause)
+	self:SetScript('OnLeave', FadeOut)
+	self:UpdateDisplay()
 end
 
 function Broker_Equipment:UNIT_INVENTORY_CHANGED(unit)
 	if(unit == 'player') then
-		UpdateDisplay()
+		self:UpdateDisplay()
 	end
 end
 
