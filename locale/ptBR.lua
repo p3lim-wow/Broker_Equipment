6a7dba386c1eff8b25dde3b9330b59a8e54a3670
Adrian L Lange <mail@p3lim.net>
2018-06-09 18:20:45 +0200

    Deal with translations ourselves

---
+++ locale/ptBR.lua
@@ -0,0 +1,13 @@
+local L = select(2, ...)('ptBR')
+
+-- tooltips (colors to indicate button or destructive action)
+-- L['|cff33ff33Left-Click|r to open equipment menu.'] = '' -- MISSING!
+-- L['|cff33ff33Right-Click|r to open character window.'] = '' -- MISSING!
+-- L['|cff33ff33Click|r to equip set.'] = '' -- MISSING!
+-- L['|cff33ff33Shift-Click|r to update set with current equipment.'] = '' -- MISSING!
+-- L['|cff33ff33Ctrl-Click|r to |cffff3333delete|r set.'] = '' -- MISSING!
+
+-- config
+-- L['Equipment sets!'] = '' -- MISSING!
+-- L['Enable tooltips in display'] = '' -- MISSING!
+-- L['Enable tooltips in menu'] = '' -- MISSING!
